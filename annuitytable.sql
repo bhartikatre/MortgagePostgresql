3e515a0c5ae11698d11f52530d2a13e22846d871
bhartikatre <74614660+bhartikatre@users.noreply.github.com>
2020-12-18 09:04:41 +0200

    annuity table defination

---
+++ annuitytable.sql
@@ -0,0 +1,3 @@
+create table annuity (Nr Integer,Payment_month text,Loan_amount€ numeric(10,2),Interest€ numeric(10,2),
+Principal_payment€ numeric(10,2),Monthly_payment€ numeric(10,2),Principal_remaining€ numeric(10,2));
+insert into annuity values (0,'0',0,0,0,0,0);
